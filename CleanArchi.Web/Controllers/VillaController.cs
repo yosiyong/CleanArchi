d6fd466b37f31e1e32cc193c8b77b8f11de78429
YongHun Lee <yosiyong@gmail.com>
2024-01-13 13:23:35 +0900

    DbContext追加、Validationメッセージをリーソスにしてカスタマイズメッセージに。

---
+++ CleanArchi.Web/Controllers/VillaController.cs
@@ -0,0 +1,40 @@
+﻿using CleanArchi.Domain.Entities;
+using CleanArchi.Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchi.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var villas = _db.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+    }
+}
